760ef8852ae01cc3306222da35a3ab34458ef99a
renzosayo <71843954+renzosayo@users.noreply.github.com>
2024-01-30 21:40:20 +0800

    Finish buildTree function

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,25 +1,38 @@
 class Node {
-  constructor (value, leftChild, rightChild) {
-    this.value = value;
-    this.leftChild = leftChild;
-    this.rightChild = rightChild;
+  constructor (data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
   }
 }
 
 class Tree {
   constructor (array) {
-    this.root = this.buildTree(array);
+    const sorted = this.sort(array);
+    const filtered = this.filter(sorted);
+    this.root = this.buildTree(filtered);
   }
 
   buildTree (array) {
-    const sorted = this.sort(array);
-    const filtered = this.filter(sorted);
-    console.log(filtered);
+    // buildTree creates a node for every call, and returns that node
+    // node contains value, left and right children
+    if (array.length === 0) {
+      return null;
+    }
+    if (array.length === 1) {
+      return new Node(array[0]);
+    }
+    let mid = Math.floor(array.length / 2);
+    const root = new Node(array[mid], 
+      this.buildTree(array.slice(0, mid)), 
+      this.buildTree(array.slice(mid + 1))
+    );
+    return root;
   }
 
   filter (array) {
     return array.filter((element, index) => {
-      
+
       // indexOf only returns 1st instance
       return array.indexOf(element) === index;
     });
@@ -46,4 +59,19 @@ class Tree {
   }
 }
 
-let tree = new Tree([2, 3, 12, 8, 4, 2]);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let tree = new Tree([2, 8, 12, 4, 34, 9, 8]);
+
+prettyPrint(tree.root);
